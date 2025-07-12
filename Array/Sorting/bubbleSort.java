0794bea8a631dd0feef9cbf95442cb108b890f26
Payel2006 <mallickkoyel098@gmail.com>
2025-07-12 09:20:27 +0530

    Create bubbleSort.java

--- Array/Sorting/bubbleSort.java
+++ Array/Sorting/bubbleSort.java
@@ -1,1 +1,24 @@
-
+class Main {
+    public static void bubbleSort(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={5,4,1,3,2};
+        bubbleSort(arr);
+        printArr(arr);
+    }
+}
