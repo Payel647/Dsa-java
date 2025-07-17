95bf5edf423716440247517c4b6e9ee4268fdeb0
Payel2006 <mallickkoyel098@gmail.com>
2025-07-17 10:27:06 +0530

    Create nto1.java

---
+++ Recursion/nto1.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void dec(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        dec(n-1);
+    }
+    public static void main(String[] args) {
+        dec(5);
+    }
+}
