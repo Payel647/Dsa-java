08742341501d54d584da37cd0e6f25918690d253
Payel2006 <mallickkoyel098@gmail.com>
2025-10-02 12:47:45 +0600

    Create LinkedHashSet.java

---
+++ Hashing/LinkedHashSet.java
@@ -0,0 +1,29 @@
+//HashSet stores single data in order (based on insertion order) and null you can add 
+import java.util.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+public class Hashing{
+    public static void main(String args[]){
+        LinkedHashSet<Integer>hs=new LinkedHashSet<>();
+        hs.add(34);
+        hs.add(12);
+        hs.add(23);
+        hs.add(67);
+        System.out.println(hs);//output->[34, 12, 23, 67]
+        System.out.println(hs.contains(100));//if contain return true else false
+        System.out.println(hs.remove(34));//return false if the key not exists else return true
+        System.out.println(hs);
+        System.out.println(hs.isEmpty());
+        hs.clear();
+        System.out.println(hs.size());
+        //option-1
+        Iterator it=hs.iterator();
+        while(it.hasNext()){
+           System.out.println(it.next());
+        }
+        //option-2
+        for(Integer i:hs){
+            System.out.println(i);
+        }
+    }
+}
