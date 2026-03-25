8da8518be3a9a2ec9622020c206f434d823e851d
Payel2006 <mallickkoyel098@gmail.com>
2026-03-25 09:32:05 +0530

    Add files via upload

---
+++ Array/Sorting/Main.java
@@ -0,0 +1,89 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+    // int arr[]={12,10,34,8};
+    // Arrays.sort(arr);
+    // Integer arr[]={12,10,34,8};
+    // Arrays.sort(arr,Comparator.reverseOrder());
+    // String arr[]={"payel","koyel","dibyendu","ayon"};
+    // Arrays.sort(arr);
+    // String arr[]={"payel","dibyendu","ayon","koyel"};
+    // Arrays.sort(arr,Comparator.reverseOrder());
+    // int arr[][]={{1,2},{0,2},{3,4}};
+    // Arrays.sort(arr,(a,b)->a[0]-b[0]);//asc
+    // int arr[][]={{1,2},{0,2},{3,4}};
+    // Arrays.sort(arr,(a,b)->b[0]-a[0]);//desc
+    // int arr[][]={{1,2},{0,2},{3,4}};
+    // Arrays.sort(arr,(a,b)->b[1]-a[1]);//based on 2nd index
+    // int arr[][]={{1,2},{0,2},{3,4}};
+    // Arrays.sort(arr,(a,b)->{
+    //     if(a[1]==b[1]){
+    //         return a[0]-b[0];//asc based on 1st index
+    //     }
+    //     return b[1]-a[1];
+    // });
+    // String arr[][]={{"payel","koyel"},{"dibyendu","ayon"},{"papiya","dipak"}};
+    // Arrays.sort(arr,(a,b)->{
+    //     if(a[1].equals(b[1])){
+    //         return a[0].compareTo(b[0]);//asc based on 1st index
+    //     }
+    //     return a[1].compareTo(b[1]);
+    // });
+    //    ArrayList<ArrayList<Integer>>ans=new ArrayList<>();
+    //    ArrayList<Integer> list1 = new ArrayList<>();
+    //    list1.add(12);
+    //    list1.add(23);
+    //    ans.add(list1);
+    //    ArrayList<Integer> list2 = new ArrayList<>();
+    //    list2.add(23);
+    //    list2.add(12);
+    //    ans.add(list2);
+    //    ArrayList<Integer> list3 = new ArrayList<>();
+    //    list3.add(10);
+    //    list3.add(9);
+    //    ans.add(list3);
+    //    Collections.sort(ans);
+    // Collections.sort(ans,Comparator.reverseOrder());
+    // Collections.sort(ans,(a,b)->{
+    //     return a.get(0)-b.get(0);
+    // });
+    //    for(ArrayList<Integer> i:ans){
+    //     System.out.println(i.get(0)+" "+i.get(1));
+    //    }
+    // class Student implements Comparable<Student>{
+    //     String name;
+    //     int roll;
+    //     Student(String name,int roll){
+    //         this.name=name;
+    //         this.roll=roll;
+    //     }
+    //     @Override
+    //     public int compareTo(Student s){
+    //         return this.roll-s.roll;
+    //     }
+    // }
+    // Student s1=new Student("payel",45);
+    // Student s2=new Student("ayon",23);
+    // Student s3=new Student("koyel",34);
+    // ArrayList<Student>ans=new ArrayList<>();
+    // ans.add(s1);
+    // ans.add(s2);
+    // ans.add(s3);
+    // Collections.sort(ans,(a,b)->{
+    //    return a.roll-b.roll;
+    // });
+    // for(Student s:ans){
+    //     System.out.println(s.name);
+    // }
+    // PriorityQueue<Student>pq=new PriorityQueue<>((a,b)->{
+    //     return a.roll-b.roll;
+    // });
+    // PriorityQueue<Student>pq=new PriorityQueue<>();
+    // pq.add(s1);
+    // pq.add(s2);
+    // pq.add(s3);
+    // while(!pq.isEmpty()){
+    //     System.out.println(pq.remove().name);
+    // }
+  }
+}
